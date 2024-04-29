ca18a5e36d6ded92258eefcb2d6c915cecae06cc
Felipe Carvalho Prette <61854726+darkLoki3@users.noreply.github.com>
2024-04-29 17:09:10 -0300

    Delete React directory

--- React/Typescript.ts
+++
@@ -1,8 +0,0 @@
-const Project_Status = ["aprovado", "reprovado", "pendente"] as const
-type ProjectStatus = (typeof Project_Status)[number]
-
-const verification = (ProjectProgressStatus: ProjectStatus): void => {
-if (Project_Status.includes(ProjectProgressStatus)) {
-console.log("contém status")
-}
-}
--- React/map_typescript.ts
+++
@@ -1,8 +0,0 @@
-const MyComponentCorrect = () => {
-  const itens = ["Item 1", "Item 2", "Item 3"];
-  return (
-    <div>
-    {itens.map((item, index) => <div key={index}>{item}</div>)}
-    </div>
-    );
-}
